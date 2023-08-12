8992b5afd7e541edc176ab5e4736ead04b074769
Yuchen Huang <charinhuang@gmail.com>
2023-08-12 10:34:09 -0700

    Created the starting project

--- WallDetect2/ViewController.swift
+++ WallDetect2/ViewController.swift
@@ -2,6 +2,8 @@
 //  ViewController.swift
 //  WallDetect2
 //
+//  This is our starting project with only a ball created at first
+//
 //  Created by Yuchen Huang on 8/8/23.
 //
 
@@ -10,39 +12,33 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let ballRadius: CGFloat = 0.02
+        let ball = SCNSphere(radius: ballRadius)
+        let ballMaterial = SCNMaterial()
+        ballMaterial.diffuse.contents = UIColor.systemPink
+        ball.materials = [ballMaterial]
+        let ballNode = SCNNode(geometry: ball)
+        sceneView.scene.rootNode.addChildNode(ballNode)
         
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
+        }
+        
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
 
